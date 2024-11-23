5baf241616c0d2efe53e3fb9b160259883cf09a9
Mauricio <mauriciofagner2010@hotmail.com>
2024-11-23 01:05:22 -0300

    Inicializar com o cabeçalho do projeto

---
+++ bignumber.h
@@ -0,0 +1,25 @@
+#ifndef bignumber_h
+#define bignumber_h
+
+typedef struct Node {
+    char digit;
+    struct Node *next_digit;
+    struct Node *prev_digit;
+};
+
+typedef struct BigNumber {
+    int is_positive;
+    Node *first_digit;
+    Node *last_digit;
+};
+
+BigNumber* create_big_number(char *str_number);
+BigNumber* sum_big_numbers(BigNumber *x, BigNumber *y);
+BigNumber* subtract_big_numbers(BigNumber *x, BigNumber *y);
+BigNumber* divide_big_numbers(BigNumber *x, BigNumber *y);
+BigNumber* multiply_big_numbers(BigNumber *x, BigNumber *y);
+
+void print_big_number(BigNumber *x);
+void free_big_number(BigNumber *x);
+
+#endif
