96e23a4f52a1f79a81052138449c6a9b058cdeb5
martinsfgr <mauriciofagner2010@hotmail.com>
2024-11-30 23:15:30 -0300

    Criar função para printar um Big Number

--- bignumber.c
+++ bignumber.c
@@ -45,6 +45,16 @@ BigNumber* create_big_number(char *str_number) {
     return big_number;
 }
 
+void print_big_number(BigNumber *big_number) {
+    if ((big_number->is_positive = 0)) printf("-");
+
+    Node* current_node = big_number->first_digit;
+    while (current_node != NULL) {
+        printf("%d", current_node->digit);
+        current_node = current_node->next_digit;
+    }
+}
+
 BigNumber* sum_big_numbers(BigNumber *x, BigNumber *y) {
     BigNumber* result = create_big_number("");
 
@@ -56,28 +66,28 @@ BigNumber* sum_big_numbers(BigNumber *x, BigNumber *y) {
     while (node_x != NULL || node_y != NULL || carry_digit > 0) {
         int digit_x, digit_y, sum, result_digit;
 
+        // Atualizando os dígitos a partir do nó atual
         digit_x = (node_x != NULL) ? node_x->digit : 0;
         digit_y = (node_y != NULL) ? node_y->digit : 0;
 
+        // Realizando a soma entre os dois dígitos, verificando o total do dígito atual e o dígito de transporte
         sum = digit_x + digit_y + carry_digit;
-        printf("Dígito de transporte atual é: %d\n", carry_digit);
-        printf("Dígito X atual é: %d\n", digit_x);
-        printf("Dígito Y atual é: %d\n", digit_y);
-
         carry_digit = sum / 10;
         result_digit = sum % 10;
 
+        // Criando o nó para o dígito do resultado da soma atual
         Node* new_node = (Node*)malloc(sizeof(Node));
-        new_node->digit = result_digit; // Criando um nó com o novo dígito a ser adicionado no resultado
+        new_node->digit = result_digit;
         new_node->next_digit = result->first_digit; // Para o novo nó, marcar o, até então, primeiro dígito do resultado, como o próximo nó dele
         new_node->prev_digit = NULL; // Inicializando o dígito anterior do nó como nulo, caso esse nó realmente tenha um dígito anterior, vai ser preenchido na próxima repetição
 
+        // Atualizando o resultado final
         if (result->first_digit == NULL) result->last_digit = new_node; // Se é o primeiro dígito, também é o último
-        if (result->first_digit != NULL) result->first_digit->prev_digit = new_node; // Adicionando o novo nó como o nó anterior, caso exista algum dígito no resultado
-
+        if (result->first_digit != NULL) result->first_digit->prev_digit = new_node; // Atualizando o novo nó como o nó anterior do último nó adicionado
+        
         result->first_digit = new_node; // Atualizando o primeiro dígito do resultado
         
-        // Atualizando os dígitos para a próxima iteração
+        // Atualizando os nós dos dígitos para a próxima iteração
         if (node_x != NULL) node_x = node_x->prev_digit;
         if (node_y != NULL) node_y = node_y->prev_digit;
     }
--- client.c
+++ client.c
@@ -3,31 +3,18 @@
 #include "bignumber.h"
 
 int main() {
-    char *str_number = "1";
+    char *str_number = "-2012";
     BigNumber* big_number = create_big_number(str_number);
-
-    Node* current_node = big_number->first_digit;
-
-    while (current_node != NULL) {
-        printf("%d", current_node->digit);
-        current_node = current_node->next_digit;
-    }
+    print_big_number(big_number);
 
     printf("\n");
 
-    printf("Soma de dois BigNumbers:\n");
+    printf("Soma de dois Big Numbers:\n");
 
     BigNumber* x = create_big_number("111");
     BigNumber* y = create_big_number("10");
     BigNumber* sum = sum_big_numbers(x, y);
-
-    current_node = sum->first_digit;
-
-    while (current_node != NULL) {
-        printf("%d", current_node->digit);
-        current_node = current_node->next_digit;
-    }
-
+    print_big_number(sum);
     printf("\n");
 
     return 0;
